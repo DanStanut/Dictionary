3208a30f158be39a82116a708c4300a48292406f
DanStanut <dan.stanut@gmail.com>
2023-10-10 10:40:44 +0300

    functional

---
+++ script.js
@@ -0,0 +1,25 @@
+const dictionary = []
+
+function resetField() {
+    document.getElementById("givenWord").value = ""
+}
+
+function addWord() {
+    let currentWord = document.getElementById("givenWord").value;
+    if (dictionary.includes(currentWord.toLowerCase())) {
+        alert("The word already exists!")
+        resetField()
+    } else {
+        dictionary.push(currentWord.toLowerCase())
+        resetField()
+    }
+}
+
+function checkWord() {
+    let currentWord = document.getElementById("givenWord").value;
+    if (dictionary.includes(currentWord.toLowerCase())) {
+        alert("The word exists!")
+    } else {
+        alert("The word doesn't exist!")
+    }
+}
