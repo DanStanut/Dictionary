b5f8878c610749e150809443609df01c77309a6b
Dan STANUT <60068096+DanStanut@users.noreply.github.com>
2023-10-11 19:27:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const dictionary = []
+const dictionary = [];
 
 function resetField() {
-    document.getElementById("givenWord").value = ""
+    document.getElementById("givenWord").value = "";
 }
 
 function addWord() {
     let currentWord = document.getElementById("givenWord").value;
     if (dictionary.includes(currentWord.toLowerCase())) {
-        alert("The word already exists!")
+        alert("The word already exists!");
     } else {
-        dictionary.push(currentWord.toLowerCase())
+        dictionary.push(currentWord.toLowerCase());
     }
-    resetField()
+    resetField();
 }
 
 function checkWord() {
     let currentWord = document.getElementById("givenWord").value;
     if (dictionary.includes(currentWord.toLowerCase())) {
-        alert("The word exists!")
+        alert("The word exists!");
     } else {
-        alert("The word doesn't exist!")
+        alert("The word doesn't exist!");
     }
 }
